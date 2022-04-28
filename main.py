5d8e54a5755fc48a2ac06b74ab1d8631e2ca1ceb
Mike <ms53150@gmail.com>
2022-04-27 21:11:53 -0400

    Add examples of creating layouts

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from tkinter import *
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-
-# Label
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-# makes label appear on screen - won't appear without .pack()
-my_label.pack()
-
-my_label["text"] = "New text"
-# my_label.config(text="New Text")
 
 # Button
 def button_clicked():
@@ -19,14 +8,31 @@ def button_clicked():
     my_label.config(text=new_text)
     print("I got clicked")
 
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+# Label
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+# my_label["text"] = "New text"
+my_label.config(text="New Text", padx=50, pady=50)
+# makes label appear on screen - won't appear without .pack()
+# my_label.pack()
+# my_label.place(x=100, y=200)
+my_label.grid(column=0, row=0)
+
 # use command to run functions
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+new_button = Button(text="New Button")
+# button.pack()
+button.grid(column=1, row=1)
+new_button.grid(column=2, row=0)
 
 # Entry
 input = Entry(width=10)
-input.pack()
-
-
+# input.pack()
+input.grid(column=3, row=2)
 
 window.mainloop()
